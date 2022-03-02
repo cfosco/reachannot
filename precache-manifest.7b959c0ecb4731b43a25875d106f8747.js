76a38ec8809ffec33a46e2ead9e9f85535c09930
Camilo Fosco <camilofosco@gmail.com>
2022-03-02 01:57:42 -0500

    Updates

---
+++ precache-manifest.7b959c0ecb4731b43a25875d106f8747.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f632c0a65f40899001ac310ea56def",
+    "url": "/reachannot/index.html"
+  },
+  {
+    "revision": "c00e55914e27c5c480b4",
+    "url": "/reachannot/static/css/2.fee219a3.chunk.css"
+  },
+  {
+    "revision": "1fcd8abcda9a2b0052f4",
+    "url": "/reachannot/static/css/main.1dd51469.chunk.css"
+  },
+  {
+    "revision": "c00e55914e27c5c480b4",
+    "url": "/reachannot/static/js/2.97254165.chunk.js"
+  },
+  {
+    "revision": "a1a4c48b83cd412bf66b4b090666c7ae",
+    "url": "/reachannot/static/js/2.97254165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcd8abcda9a2b0052f4",
+    "url": "/reachannot/static/js/main.9112a49a.chunk.js"
+  },
+  {
+    "revision": "a20cd26a4109ddd74cb2",
+    "url": "/reachannot/static/js/runtime-main.089b46f7.js"
+  },
+  {
+    "revision": "5446139eaa88f2160bc2d66ceb7d72fa",
+    "url": "/reachannot/static/media/allControls.5446139e.png"
+  },
+  {
+    "revision": "958d15dc0971095be2555128add6e179",
+    "url": "/reachannot/static/media/annotationIllustration_interactability.958d15dc.png"
+  },
+  {
+    "revision": "19bfc913e22d86646111ca02b17c8b44",
+    "url": "/reachannot/static/media/guidelines.19bfc913.png"
+  },
+  {
+    "revision": "41369ade4d202c5f4f9df918eef820ef",
+    "url": "/reachannot/static/media/panes.41369ade.png"
+  }
+]);
